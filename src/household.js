575413cee85c000f9fe37b3e60e7039cb95c648d
Peter Grimm <pagrimm@gmail.com>
2020-07-06 11:49:05 -0700

    add passing tests for creating household object and adding roommates to household

--- src/household.js
+++ src/household.js
@@ -1,9 +1,17 @@
 //Household = {this.roomates = []; Expenses = []; Total = num;}
+import { Roommate } from './roommate';
 
 export class Household {
   constructor() {
     this.roommates = [];
     this.expenses = [];
     this.total = 0;
+    this.nextIndex = 0;
+  }
+  
+  addRoommate (name) {
+    let roommate = new Roommate(name, this.nextIndex);
+    this.nextIndex ++;
+    this.roommates.push(roommate);
   }
 }
